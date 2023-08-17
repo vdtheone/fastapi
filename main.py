edae6142bb37aa7e5c3aa4eded5cb80f34f2f3cb
vdtheone <vishalprajapati22052001@gmail.com>
2023-08-17 09:48:34 +0530

    use responce model in student and percent class

--- main.py
+++ main.py
@@ -68,7 +68,21 @@ def set_rsp_headers():
    return JSONResponse(content=content, headers=headers)
 
 
+class student(BaseModel):
+   id: int
+   name :str = Field(None, title="name of student", max_length=10)
+   marks: List[int] = []
+   percent_marks: float
+   
+class percent(BaseModel):
+   id:int
+   name :str = Field(None, title="name of student", max_length=10)
+   percent_marks: float
 
+@app.post("/marks", response_model=percent)
+async def get_percent(s1:student):
+   s1.percent_marks=sum(s1.marks)/2
+   return s1
 
 
 
